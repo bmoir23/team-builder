419c44f77a91e665c1fdb2ee943e77fe46af6f4f
Brian Moir <48401267+bmoir23@users.noreply.github.com>
2019-10-10 19:20:20 -0400

    mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header/Header';
+import React, { useState } from 'react';
+import TeamMembers from './components/TeamMembers/TeamMembers';
+import Form from './components/Form/Form';
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState( [{name: 'Henry Ford', role: 'Designer'}, {name: 'Alice Liddell', role: 'Rabbit Hole Expert'}, {name: 'Charley Chaplin', role: 'Entertainer'}] );
+  const [memberName, setMemberName] = useState('');
+  const [memberRole, setMemberRole] = useState('');
+
+  // console.log(memberName)
+  // console.log(memberRole)
+
+  const nameHandler = event => {
+    setMemberName(event.target.value);
+  }
+
+  const roleHandler = event => {
+    setMemberRole(event.target.value)
+  }
+
+  const addTeamMember = member => {
+    member.preventDefault();
+    const newMember = {};
+    newMember.name = memberName;
+    newMember.role = memberRole;
+    setTeamMembers( [...teamMembers, newMember] );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Form nameHandler={nameHandler} roleHandler={roleHandler} addTeamMember={addTeamMember}/>
+      <TeamMembers teamMembers={teamMembers} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ team-builder/src/component/form/form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './form.css';
+
+const Form = (props) => {
+    // console.log(props)
+    return (
+        <div className='MemberForm'>
+            <h2>Add Team Member</h2>
+            <div className='form'>
+                <form onSubmit={event => props.addTeamMember(event)}>
+                    <label htmlFor='MemberName'>
+                        User Name   
+                    </label>
+                    <input type='text' id='MemberName' placeholder='Team Member Name...' onChange={props.nameHandler}/>
+                    <label htmlFor='MemberRole'>
+                        Role 
+                    </label>
+                    <input type='text' id='MemberRole' placeholder='Team Member Role...' onChange={props.roleHandler}/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
