db0f683d32dd849342709a1a47f7ca98440d91da
Brian Moir <48401267+bmoir23@users.noreply.github.com>
2019-10-10 20:34:18 -0400

    went different route but it worked

--- team-builder/src/components/form/form.js
+++ team-builder/src/components/form/form.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './Form.css';
+// import React from 'react';
+// import './Form.css';
 
-const Form = (props) => {
-    // console.log(props)
-    return (
-        <div className='MemberForm'>
-            <h2>Add Team Member</h2>
-            <div className='form'>
-                <form onSubmit={event => props.addTeamMember(event)}>
-                    <label htmlFor='MemberName'>
-                        User Name   
-                    </label>
-                    <input type='text' id='MemberName' placeholder='Team Member Name...' onChange={props.nameHandler}/>
-                    <label htmlFor='MemberRole'>
-                        Role 
-                    </label>
-                    <input type='text' id='MemberEmail' placeholder='Team Member Email...' onChange={props.nameHandler}/>
-                    <label htmlFor='MemberRole'>
-                       Email
-                    </label>
-                    <input type='text' id='MemberRole' placeholder='Team Member Role...' onChange={props.roleHandler}/>
-                    <button>Submit</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+// const Form = (props) => {
+//     // console.log(props)
+//     return (
+//         <div className='MemberForm'>
+//             <h2>Add Team Member</h2>
+//             <div className='form'>
+//                 <form onSubmit={event => props.addTeamMember(event)}>
+//                     <label htmlFor='MemberName'>
+//                         User Name   
+//                     </label>
+//                     <input type='text' id='MemberName' placeholder='Team Member Name...' onChange={props.nameHandler}/>
+//                     <label htmlFor='MemberRole'>
+//                         Role 
+//                     </label>
+//                     <input type='text' id='MemberEmail' placeholder='Team Member Email...' onChange={props.nameHandler}/>
+//                     <label htmlFor='MemberRole'>
+//                        Email
+//                     </label>
+//                     <input type='text' id='MemberRole' placeholder='Team Member Role...' onChange={props.roleHandler}/>
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Form;
+// export default Form;
